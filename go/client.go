af1b53a5f4d5571e5ccb8f785dae9b5ca948ceb0
cyan92128505 <cyan92128505@gmail.com>
2022-02-21 16:51:45 +0800

    :beers: Test ethereum

---
+++ go/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("we have a connection")
+	account := common.HexToAddress("0xe2D24d3AB12077B3e5961a76B0BC91798e351154")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance)
+}
